e687219b4d27cbc8183d8b1b62948b2047629f73
nandorcsupor <csuprok@hotmail.com>
2023-05-22 20:42:15 +0200

    add endpoint and stuff

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Set CORS configurations
+origins = [
+    "http://localhost:3000",
+    # Add other allowed origins here
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class MatchCreateRequest(BaseModel):
     max_tickets: int
@@ -15,18 +30,20 @@ class MatchCreateRequest(BaseModel):
     category: int
 
 @app.post('/create-match')
-def create_match(match_data: MatchCreateRequest):
+def create_match(match_data: dict):
+    print('MATCH DATA:', match_data)
     # Extract the necessary fields from the request body
-    max_tickets = match_data.max_tickets
-    tokenName = match_data.tokenName
-    tokenSymbol = match_data.tokenSymbol
-    gate = match_data.gate
-    section = match_data.section
-    row = match_data.row
-    seat = match_data.seat
-    category = match_data.category
+    # max_tickets = match_data.max_tickets
+    # tokenName = match_data.tokenName
+    # tokenSymbol = match_data.tokenSymbol
+    # gate = match_data.gate
+    # section = match_data.section
+    # row = match_data.row
+    # seat = match_data.seat
+    # category = match_data.category
 
     # Rest of your Brownie code...
+    # deploy_ticket(max_tickets, tokenName, tokenSymbol, gate, section, row, seat, category)
 
     return {'message': 'Match created successfully'}
 
